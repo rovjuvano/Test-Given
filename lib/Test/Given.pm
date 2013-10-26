cbbd6efc879adcd99aea3116616aedcd29adc196
Robert Juliano <rovjuvano@gmail.com>
2013-10-26 17:36:25 -0400

    second attempt version 0.02

--- lib/Test/Given.pm
+++ lib/Test/Given.pm
@@ -1,22 +1,22 @@
 package Test::Given;
 use strict;
 use warnings;
-use Test::More;
-use B::Deparse;
+use Test::More qw();
+use B::Deparse ();
+use PadWalker qw(closed_over);
 use Data::Dumper;
-use PadWalker qw(set_closed_over closed_over);
-#use Storable qw(dclone);
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(describe context Given When Then And Invariant *self);
+our @EXPORT = qw(describe context Given When Then And Invariant);
 
 use version;
-our $Version = '0.01';
+our $Version = '0.02';
 
-our $self;
 my @suites = ();
 my $and_parent = \&Given;
+our $package;
+my $context_vars = {};
 
 my @givens = ();
 my @whens = ();
@@ -27,25 +27,46 @@ sub describe {
   push @suites, 1;
   $block->();
   pop @suites;
-  done_testing() unless @suites;
+  Test::More::done_testing() unless @suites;
 }
 
 sub make_given_when_block {
   my $sub = pop;
   my $name = pop;
-  return sub {
-    my $value = $sub->();
-    $self->{$name} = $value if $name;
-  };
+  if ( defined $name ) {
+    my $package = "${package}::";
+    if ( $name =~ s/^\@// ) {
+      return sub {
+        define_var($package, $name, [ $sub->() ]);
+      }
+    }
+    elsif ( $name =~ s/^\%// ) {
+      return sub {
+        define_var($package, $name, { $sub->() });
+      }
+    }
+    elsif ( $name =~ s/^\&// ) {
+      return sub {
+        define_var($package, $name, $sub->());
+      }
+    }
+    $name =~ s/^\$//;
+    return sub {
+      define_var($package, $name, \$sub->());
+    }
+  }
+  return sub { $sub->() }
 }
 
 sub Given {
   $and_parent = \&Given;
+  local ($package) = $package || (caller);
   push @givens, make_given_when_block(@_);
 }
 
 sub When {
   $and_parent = \&When;
+  local ($package) = $package || (caller);
   push @whens, make_given_when_block(@_);
 }
 
@@ -54,22 +75,23 @@ sub Invariant {
   push @invariants, $_[0];
 }
 
+sub call_sub { $_[0]->() }
 sub Then {
   $and_parent = \&Then;
   my ($sub) = @_;
+  reset_context();
+  map { call_sub($_) } @givens;
+  map { call_sub($_) } @whens;
+  my $passed = eval { call_sub($sub); };
+  $passed = '' if $@;
   my $tb = Test::More->builder;
   $tb->level( $tb->level() + 1 );
-  my $passed = sub {
-    $self = {};
-    map { $_->() } @givens;
-    map { $_->() } @whens;
-    $sub->();
-  }->();
-  ok($passed, message($sub));
+  Test::More::ok($passed, message($sub));
   $tb->level( $tb->level() - 1 );
 }
 
 sub And {
+  local ($package) = (caller);
   my $tb = Test::More->builder;
   $tb->level( $tb->level() + 1 );
   $and_parent->(@_);
@@ -80,24 +102,67 @@ my $deparser = B::Deparse->new();
 sub message {
   my ($sub) = @_;
 
-  my ($code) = $deparser->coderef2text($sub);
-  $code =~ s/^\{|\n\}$//g;
-  $code =~ s/\$(\$.*?)\{/$1->\{/g;
+  my $message = "Then returned false:\n";
+
+  my @code = split( /\n/, $deparser->coderef2text($sub) );
+  @code = (@code > 1) ? @code[1..$#code-1] : ();
+  @code = map {
+    s/\$(\$.*?)\{/$1->\{/g;
+    $_;
+  } grep { !/^ *(?:package|use) / } @code;
+
+  $message .= join("\n", @code);
 
   local $Data::Dumper::Indent = 1;
   my (@vars) = split( /\n/, Dumper((closed_over($sub))[0]) );
   @vars = (@vars > 1) ? @vars[1..$#vars-1] : ();
   @vars = map { s/^  '(.*)' => \\/    $1 = /; $_ } @vars;
+  $message .= join("\n", "\n\n Variables:", @vars);
 
-  $_ = $code;
-  while (my ($key) = /\$self->{'(.*?)'}/) {
-    s///;
-    my $line = '    ' . Data::Dumper->Dump([$self->{$key}], ["self->{'$key'}"]);
-    chomp($line);
-    push(@vars, $line);
+#  $_ = $code;
+#  while (my ($key) = /\$self->{'(.*?)'}/) {
+#    s///;
+#    my $line = '    ' . Data::Dumper->Dump([$self->{$key}], ["self->{'$key'}"]);
+#    chomp($line);
+#    push(@vars, $line);
+#  }
+
+  return $message;
+}
+
+sub reset_context {
+  foreach my $package (keys %$context_vars) {
+    no strict 'refs';
+    my $sym_tab = *{$package}{HASH};
+    foreach my $name (keys %{ $context_vars->{$package}}) {
+      undef *{$package . $name};
+    }
   }
-  
-  join("\n", "Then returned false:$code", "\n  Variables:", @vars, "\n  ");
+}
+
+sub define_var {
+  my ($package, $name, $value) = @_;
+  return unless $name;
+  $context_vars->{$package}->{$name} = $value;
+  no strict 'refs';
+  *{$package . $name} = $value;
+}
+
+sub dump_symbol_table {
+  my ($table) = @_;
+  my %h;
+  no strict 'refs';
+  foreach my $key (keys %$table) {
+    $h{$key} = {};
+    my $v = *{ $table->{$key} }{SCALAR};
+    $h{$key}->{SCALAR} = $v if defined $$v;
+    map {
+      my $v = *{ $table->{$key} }{$_};
+      $h{$key}->{$_} = $v if defined $v;
+    } qw(ARRAY HASH CODE FORMAT IO);
+  }
+  use strict 'refs';
+  warn Data::Dumper->new([\%h])->Deepcopy(1)->Sortkeys(1)->Dump();
 }
 
 1;
--- t/syntax.t
+++ t/syntax.t
@@ -1,19 +1,26 @@
+package myp;
 use Test::Given;
+use strict;
+use warnings;
 
+our ($name, $other, @array, %hash, $result, $result2, $self);
 my $a = 1;
 my $b = 2;
 my $c;
 describe 'Subject Under Test' => sub {
-  Given 'name' => sub { 'value' };
-   And 'other' => sub { 'another value' };
+  Given name => sub { 'value' };
+   And other => sub { 'another value' };
    And sub { $self->{'added-given'} = 'yes' };
-  When 'result' => sub { 'result' };
-   And 'result2' => sub { 'result2' };
+   And '@array' => sub { (1, 2, 3) };
+   And '%hash' => sub { (a => 1, b => 2) };
+   And '&func' => sub { sub { 'hi' } };
+  When result => sub { 'something' };
+   And '$result2' => sub { 'something2' };
    And sub { $self->{'added-when'} = 'yes' };
   Then sub { $a == 2 };
    And sub { $a == $b };
-   And sub { $self->{name} eq $b };
-   And sub { $self->{other} eq 'value' };
-   And sub { $self->{other} eq $self->{result} };
-   And sub { use Data::Dumper; warn Dumper($self) };
+   And sub { $name eq 'value' };
+   And sub { $other eq 'another value' };
+   And sub { $self->{other} eq $result };
+   And sub { use Data::Dumper; warn Dumper(\@array, \%hash, \&func) };
 };
