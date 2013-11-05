8222edd5df1d06c9a3a8842ab845a17490be4e94
Robert Juliano <rovjuvano@gmail.com>
2013-11-04 19:35:14 -0500

    prove command not available on CPAN testers

--- t/test_helper.pl
+++ t/test_helper.pl
@@ -1,11 +1,13 @@
 use IPC::Open3;
+use Config;
 use strict;
 use warnings;
 
+my @prove = ($Config{perlpath}, '-mApp::Prove', '-e', '$a=App::Prove->new();$a->process_args(@ARGV);$a->run()', '--', '-v');
 sub run_spec {
   my ($filename) = @_;
   local $? = 0;
-  my $pid = open3(\*IN, \*OUT, \*OUT, qw(prove -v --lib), $filename);
+  my $pid = open3(\*IN, \*OUT, \*OUT, @prove, $filename);
   close(IN);
   my @lines = <OUT>;
   close(OUT);
